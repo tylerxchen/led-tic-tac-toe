fc88597e7757bde40ceba62c5c7c4a177f545ea1
Tyler Chen <tylerc6@illinois.edu>
2019-04-18 20:33:38 -0500

    committed some encoder code

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,17 +1,64 @@
 // pin macros
+const int CLK = 2;
+const int DT = 3;
 
 // variables
 int game[3][3]; // empty represented by 0, red represented by 1, blue represented by 2
+int turn;
+int cursorCol;
+int cursorRow;
+int gameChanged;
+int changePos;
 
 void setup()
 {
+    Serial.begin(9600);
+    pinMode(CLK, INPUT);
+    pinMode(DT, INPUT);
     memset(game, 0, sizeof(game)*3*3);
-    
+    turn = 1;
+    cursorCol = 0;
+    cursorRow = 0;
+    gameChanged = 0;
+    changePos = 0;
+    printBoard();
+    attachInterrupt(digitalPinToInterrupt(CLK), readEncoder, RISING);
 }
 
 void loop()
 {
-    
+    if (changePos) {
+	cursorRow += (cursorCol + changePos) / 3;
+	cursorCol = (cursorCol + changePos) % 3;
+	changePos = 0;
+	gameChanged = 1;
+    }
+    if (gameChanged) {
+	printBoard();
+	gameChanged = 0;
+    }
+}
+
+void printBoard()
+{
+    for (int i = 0; i < 3; i++) {
+	for (int j = 0; j < 3; j++) {
+	    Serial.print(game[i][j]);
+	    if (i == cursorRow && j == cursorCol)
+		Serial.print("*");
+	}
+	Serial.println();
+    }
+}
+
+void readEncoder()
+{
+    if (digitalRead(DT) != digitalRead(CLK)) {
+	changePos = 1;
+    }
+    else {
+	changePos = -1;
+    }
 }
 
 void mark(int game[3][3], int i, int j, int player)
@@ -19,6 +66,8 @@ void mark(int game[3][3], int i, int j, int player)
     if (!game[i][j]) {
 	game[i][j] = player;
     }
+
+    gameChanged = 1;
 }
 
 int check_win(int game[3][3])
