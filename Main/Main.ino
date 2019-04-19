0c6c624e11702888b8583007d101cb7f6c18662f
Tyler Chen <tylerc6@illinois.edu>
2019-04-19 17:42:27 -0500

    working display code

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,6 +1,15 @@
-// pin macros
+#include <TimedAction.h>
+
+// pin constants
 const int CLK = 2;
 const int DT = 3;
+const int SHIFT_RCLK = 4;
+const int SHIFT_SRCLK = 5;
+const int SHIFT_SER = 6;
+const int COL1 = 7;
+const int COL2 = 8;
+const int COL3 = 9;
+
 
 // variables
 int game[3][3]; // empty represented by 0, red represented by 1, blue represented by 2
@@ -8,25 +17,97 @@ int turn;
 int cursorCol;
 int cursorRow;
 int gameChanged;
+int column;
 volatile int changePos;
+
+void displayBoard()
+{
+	digitalWrite(COL1, HIGH);
+	digitalWrite(COL2, HIGH);
+	digitalWrite(COL3, HIGH);
+	digitalWrite(SHIFT_SRCLK, LOW);
+	digitalWrite(SHIFT_RCLK, LOW);
+	
+	for (int i = 0; i < 3; i++) {
+		switch (game[i][column]) {
+			case 0:
+			digitalWrite(SHIFT_SER, LOW);
+			digitalWrite(SHIFT_SRCLK, HIGH);
+			digitalWrite(SHIFT_SRCLK, LOW);
+			digitalWrite(SHIFT_SER, LOW);
+			digitalWrite(SHIFT_SRCLK, HIGH);
+			digitalWrite(SHIFT_SRCLK, LOW);
+			break;
+			
+			case 1:
+			digitalWrite(SHIFT_SER, LOW);
+			digitalWrite(SHIFT_SRCLK, HIGH);
+			digitalWrite(SHIFT_SRCLK, LOW);
+			digitalWrite(SHIFT_SER, HIGH);
+			digitalWrite(SHIFT_SRCLK, HIGH);
+			digitalWrite(SHIFT_SRCLK, LOW);
+			break;
+			
+			case 2:
+			digitalWrite(SHIFT_SER, HIGH);
+			digitalWrite(SHIFT_SRCLK, HIGH);
+			digitalWrite(SHIFT_SRCLK, LOW);
+			digitalWrite(SHIFT_SER, LOW);
+			digitalWrite(SHIFT_SRCLK, HIGH);
+			digitalWrite(SHIFT_SRCLK, LOW);
+			break;
+		}
+	}
+	
+	digitalWrite(SHIFT_SER, LOW);
+	digitalWrite(SHIFT_SRCLK, HIGH);
+	digitalWrite(SHIFT_SER, LOW);
+	digitalWrite(SHIFT_SRCLK, HIGH);
+	
+	digitalWrite(SHIFT_RCLK, HIGH);
+	digitalWrite(SHIFT_RCLK, LOW);
+	
+	digitalWrite(column + 7, LOW);
+	column = (column + 1) % 3;
+}
+
+// TimedAction
+TimedAction updateDisplay = TimedAction(1, displayBoard);
 
 void setup()
 {
     Serial.begin(9600);
     pinMode(CLK, INPUT);
     pinMode(DT, INPUT);
+	pinMode(SHIFT_RCLK, OUTPUT);
+	pinMode(SHIFT_SER, OUTPUT);
+	pinMode(SHIFT_SRCLK, OUTPUT);
+	pinMode(COL1, OUTPUT);
+	pinMode(COL2, OUTPUT);
+	pinMode(COL3, OUTPUT);
     memset(game, 0, sizeof(game[0][0])*3*3);
+	//game[2][2] = 1;
+	//game[0][0] = 2;
+	game[2][2] = 2;
+	game[1][1] = 2;
+	game[0][0] = 2;
+	game[1][0] = 1;
+	game[1][2] = 1;
+	game[2][1] = 1;
+	game[0][1] = 1;
     turn = 1;
     cursorCol = 0;
     cursorRow = 0;
     gameChanged = 0;
     changePos = 0;
+	column = 0;
     printBoard();
     attachInterrupt(digitalPinToInterrupt(CLK), readEncoder, CHANGE);
 }
 
 void loop()
 {
+	updateDisplay.check();
     if (changePos) {
 	cursorRow = (cursorRow + ((cursorCol + changePos) / 3)) % 3;
 	cursorCol = (cursorCol + changePos) % 3;
@@ -51,6 +132,8 @@ void printBoard()
     }
     Serial.println();
 }
+
+
 
 void readEncoder()
 {
